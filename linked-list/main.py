dfc44d14492ce90df195ea970cc8fcb098cc7f48
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2026-03-19 22:51:32 +0530

    added the remove

--- linked-list/main.py
+++ linked-list/main.py
@@ -1,33 +1,47 @@
-# Node class
+# Node Class
 
 class Node:
-  def __init__(self, value, next = None):
+  def __init__(self, value, next=None):
     self.value = value
     self.next = next
-
-#Linked List
+  
+# Node LinkedList
 
 class LinkedList:
-  def __init__(self):
-    self.head = None
+  def __init__(self, head=None):
+    self.head = head
   
   def insert(self, value):
     new_node = Node(value)
     new_node.next = self.head
     self.head = new_node
-
-  def print_list(self):
+  
+  def print(self):
     current = self.head
     while current:
       print(current.value)
+      current = current.next
+  
+  def remove(self, value):
+    current = self.head
+
+    if current is None:
+      return
+    
+    if current.value == value:
+      self.head = current.next
+      return
+    
+    while current.next:
+      if current.next.value == value:
+        current.next = current.next.next
+        return
       current = current.next
 
 ll = LinkedList()
 
 ll.insert(5)
 ll.insert(10)
-ll.insert(21)
-
-
-
-ll.print_list()
+ll.insert(100)
+ll.remove(10)
+ll.print()
