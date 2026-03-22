d83b43bf489ebab3a10cd992e580ff3c0037c921
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2026-03-22 23:23:24 +0530

    swap node added

--- linked-list/main.py
+++ linked-list/main.py
@@ -1,35 +1,25 @@
-# Node Class
+# Node class
 
 class Node:
-  def __init__(self, value, next=None):
+  def __init__(self, value, next = None):
     self.value = value
     self.next = next
-  
-# Node LinkedList
+
+# Linked list
 
 class LinkedList:
-  def __init__(self, head=None):
+  def __init__(self, head = None):
     self.head = head
-  
+
   def insert(self, value):
-    new_node = Node(value)
-    new_node.next = self.head
-    self.head = new_node
-  
-  def print(self):
-    current = self.head
-    while current:
-      print(current.value)
-      current = current.next
-  
+    node_head = Node(value)
+    node_head.next = self.head
+    self.head = node_head
+
   def remove(self, value):
     current = self.head
 
-    if current is None:
-      return
-    
-    if current.value == value:
-      self.head = current.next
+    if current.value is None:
       return
     
     while current.next:
@@ -37,11 +27,72 @@ class LinkedList:
         current.next = current.next.next
         return
       current = current.next
+  
+  def swap_nodes(self, val1, val2):
+
+    # if both values are same there is nothing to worry about
+
+    if val1 == val2:
+      return
+    
+    # find val1
+
+    prev1 = None
+    curr1 = self.head
+
+    while curr1:
+      if curr1.value == val1:
+        prev1 = curr1
+        curr1 = curr1.next
+    
+    # find val2
+
+    prev2 = None
+    curr2 = self.head
+
+    while curr2:
+      if curr2.value == val2:
+        prev2 = curr2
+        curr2 = curr2.next
+    
+    if curr1 is None or curr2 is None:
+      print('One or two values are missing')
+      return
+
+    # change the previous nodes
+
+    if prev1 is None:
+      self.head = curr2
+    else:
+      prev1.next = curr2
+
+    if prev2 is None:
+      self.head = curr1
+      prev2.next = curr1
+
+    # swap next pointers
+
+    temp = curr1.next
+    curr1.next = curr2.next
+    curr2.next = temp
+
+
+  def print(self):
+    current = self.head
+
+    while current:
+      print(current.value)
+      current = current.next
+
+
 
 ll = LinkedList()
 
-ll.insert(5)
-ll.insert(10)
-ll.insert(100)
-ll.remove(10)
+ll.insert(23)
+ll.insert(65)
+ll.insert(245)
+ll.insert(52)
+
+ll.swap_nodes(65, 34)
+
 ll.print()
