424177af6e98c20ad77323f779bc7e2798537f43
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2026-03-28 21:36:17 +0530

    doubly linked list - add to head class made

--- linked-list/main.py
+++ linked-list/main.py
@@ -1,53 +1,39 @@
-# Node class
+# Node Class
 
 class Node:
-  def __init__(self, value, next = None ):
+  def __init__(self, value):
     self.value = value
-    self.next = next
+    self.next = None
+    self.prev = None 
 
-# Linked List
+# Doubly LinkdedList
 
-class Linked_list:
-  def __init__(self, head = None):
-    self.head = head
-
-  def insert(self, value):
-    new_node = Node(value)
-    new_node.next = self.head
+class DoublyLinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+  
+  def add_to_head(self, value):
+    new_node = Node (value)
+    
+    if self.head:
+      new_node.next = self.head
+      self.head.prev = new_node
+    else:
+      self.tail = new_node
+    
     self.head = new_node
-
-  def remove(self, value):
-    
-    # values are 5,4,2 and lets say we want to remove value 4
-    # 1. 4 node pointer points to the 2 node
-    # 2. we need to make 5 pointer points to the 2 node instead of the 4 node or kind of make it skip
-
-    curr = self.head
-
-    if curr.value is None:
-      return
-    
-    if curr.value == value:
-      self.head = curr.next
-      return
-      
-    
-    while curr.next:
-      if curr.next.value == value:
-        curr.next = curr.next.next
-        return 
-      curr = curr.next
-
+  
   def print(self):
     curr = self.head
+
     while curr:
       print(curr.value)
       curr = curr.next
+    
 
-ll = Linked_list()
+dll = DoublyLinkedList()
 
-ll.insert(2)
-ll.insert(5)
-ll.insert(8)
-ll.remove(8)
-ll.print()
+dll.add_to_head(2)
+dll.add_to_head(59)
+dll.print()
